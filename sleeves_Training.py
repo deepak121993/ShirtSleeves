53b9954a1ad9f9acf9c8b0377940cde80d42ab63
Deepak Saini <deepak.saini@ad.infosys.com>
2019-04-23 00:53:36 +0530

    bug

--- sleeves_Training.py
+++ sleeves_Training.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import  train_test_split
 from nn.cnn.vgg import MiniVGGNet
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
-from keras.optimizers import SGD
+from keras.optimizers import SGD,Adam
 import matplotlib.pyplot as plt
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
@@ -21,12 +21,21 @@ from keras.utils import to_categorical
 import matplotlib
 from keras.callbacks import ModelCheckpoint 
 from flask import Flask
+from keras.callbacks import LearningRateScheduler 
 # app = Flask(__name__)
 
 # if __name__== '__main__':
 #     app.run(debug="True")
 
 
+def step_decay(epohs):
+
+    initAlpha = 0.01
+    factor=0.25
+    dropEvery=5
+
+    alpha = initAlpha*(factor**np.floor((1+epohs)/dropEvery))
+    return float(alpha)
 
 matplotlib.use("Agg")
 
@@ -79,8 +88,8 @@ valGen   = HDF5DataGenerator(test_hdf5,28,aug=aug,preprocessors=[sp,iap],classes
 #testY = to_categorical(testY)
 import os
 fname=os.path.sep.join(["ShirtSleeves/model/","weight-{epoch:03d}-{val_loss:.4f}.hdf5"])
-callbacks=[ModelCheckpoint(fname,monitor="val_loss",mode="min",save_best_only=True)]
-opt = SGD(lr=5e-3, momentum=0.9, nesterov=True)
+callbacks=[ModelCheckpoint(fname,monitor="val_loss",mode="min",save_best_only=True),LearningRateScheduler(step_decay)]
+opt = Adam(lr=0.01,beta_1=0.9, beta_2=0.999, epsilon=None)
 
 model = MiniVGGNet.build(width=128, height=128, depth=3, classes=4)
 
@@ -91,7 +100,7 @@ model.compile(loss="categorical_crossentropy", optimizer=opt,
     metrics=["accuracy"])
 
 
-model.fit_generator(trainGen.generator(),steps_per_epoch= trainGen.numImages//28 ,
+H= model.fit_generator(trainGen.generator(),steps_per_epoch= trainGen.numImages//28 ,
                     validation_data = valGen.generator(),validation_steps=valGen.numImages//28,
                     verbose=1,epochs=70,callbacks=callbacks)
 
@@ -116,14 +125,14 @@ model.fit_generator(trainGen.generator(),steps_per_epoch= trainGen.numImages//28
 
 
 
-# plt.style.use("ggplot")
-# plt.figure()
-# plt.plot(np.arange(0, 10), H.history["loss"], label="train_loss")
-# plt.plot(np.arange(0, 10), H.history["val_loss"], label="val_loss")
-# plt.plot(np.arange(0, 10), H.history["acc"], label="train_acc")
-# plt.plot(np.arange(0, 10), H.history["val_acc"], label="val_acc")
-# plt.title("Training Loss and Accuracy on CIFAr-10")
-# plt.xlabel("Epoch #")
-# plt.ylabel("Loss/Accuracy")
-# plt.legend()
-# plt.savefig(args["output"])
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 70), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 70), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 70), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, 70), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy on CIFAr-10")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig(args["output"])
